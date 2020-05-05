2662ef683e7f3479979ba3984723b4de59e3f8ee
Matthew Beck <matthewbeck@nevada.unr.edu>
2020-05-05 15:33:10 -0700

    Lengthen abstract

--- src/HomeSection.tsx
+++ src/HomeSection.tsx
@@ -31,6 +31,13 @@ function HomeSection({ state }: SectionProps) {
           DeepCue uses a deep Q-learning algorithm to initially learn how to
           play a game then uses a convolutional neural network to incorporate
           user guidance in the form of game inputs into the learning process.
+          BGP++ will start by applying DeepCue to Super Mario Bros. In the case
+          of Super Mario Bros, the possible game inputs are left, right, down,
+          A, and B. The user can help the model recognize when to perform
+          actions. An example is a user pressing A to jump when Mario encounters
+          a pit. The goal of DeepCue is to show that a machine learning model
+          accepting user guidance outperforms a model without user guidance in
+          both how quickly it is trained and how well it performs.
         </p>
       </div>
       <div className="home-container">
