8a97898e2d6e413b94b783fcde085c1c5b5d6533
Price <pricep@nevada.unr.edu>
2020-05-05 19:44:08 -0700

    half the team page

--- src/TeamSection.tsx
+++ src/TeamSection.tsx
@@ -44,6 +44,47 @@ function TeamSection({ state }: SectionProps) {
                 Derek is currently an undergrad researcher in Dr. Emily Hand's lab. He has experience with Python, Tensorflow, NumPy, and Matplotlib, and he completed Dr. Hand’s machine learning class. Derek worked on conceptualizing, designing, and developing the learning algorithm.
             </p>
         </div>
+
+        {/*Row two!*/}
+
+        <div className="team-card">
+            <img src="https://www.unr.edu/main/images/colleges-schools/engineering/cse/profiles/SergiuDascalu.jpg" alt="Dascalu" className={"team-card-image"}/>
+            <h1>Sergiu Dascalu</h1>
+            <p className="team-card-title">BGP++ Mentor, Professor</p>
+            <p className="team-card-description">
+                Dascalu is a Professor in the Department of Computer Science and Engineering at the University of Nevada, Reno (UNR), which he joined in July 2002. Dascalu has a PhD degree in Computer Science (2001) from Dalhousie University, Canada and a Master’s degree in Automatic Control and Computers (1982) from the Polytechnic of Bucharest, Romania.
+            </p>
+        </div>
+
+        <div className="team-card">
+            <img src="https://www.unr.edu/main/images/colleges-schools/engineering/cse/profiles/DavidFeilSeifer.jpg" alt="Dave" className={"team-card-image daves-special-card-cause-his-name-is-too-long"}/>
+            <h1>David Feil-Seifer</h1>
+            <p className="team-card-title">BGP++ Mentor, Associate Professor, Graduate Program Director</p>
+            <p className="team-card-description">
+                David Feil-Seifer studied computer science for his undergraduate degree at the University of Rochester, M.S. and Ph.D. at the University of Southern California, and postdoctoral associate at Yale University.
+
+                He has been the leader of his undergraduate robotics competition team, robotics competition coach, and research mentor to 45 undergraduates.
+
+            </p>
+        </div>
+
+        <div className="team-card">
+            <img src="https://media-exp1.licdn.com/dms/image/C4E03AQGP-WVk-HtN5g/profile-displayphoto-shrink_200_200/0?e=1594252800&v=beta&t=LRW1S-FGoH4ODVsPKcQzPU_PR9jSZttGYQKwynvCKTE" alt="Devrin" className={"team-card-image"}/>
+            <h1>Davrin Lee</h1>
+            <p className="team-card-title">BGP++ Mentor, Operational Program Manager at Microsoft</p>
+            <p className="team-card-description">
+                Devrin works full time at Microsoft while also teaching at UNR. Devrin is an instructor for both CS 425 Software Engineering and CS 426 Sr Projects.
+            </p>
+        </div>
+
+        <div className="team-card">
+            <img src="https://www.unr.edu/main/images/colleges-schools/engineering/cse/profiles/EmilyHand.jpg" alt="Emily" className={"team-card-image"}/>
+            <h1>Emily Hand</h1>
+            <p className="team-card-title">BGP++ Mentor, Assistant Professor</p>
+            <p className="team-card-description">
+                Dr. Emily Hand is an assistant professor in the Department of Computer Science and Engineering at the University of Nevada, Reno. Dr. Hand received her Ph.D. in computer science from the University of Maryland, College Park under the supervision of Professor Rama Chellappa. She has held research positions at NASA Ames, NASA JPL and the Naval Research Laboratory.
+            </p>
+        </div>
     </section>
   );
 }
